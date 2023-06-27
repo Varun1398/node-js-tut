1fc50d46eb905dc941844a892bfde45738862338
Varun Sharma <varunsharma.32145@gmail.com>
2023-06-27 15:37:50 +0530

    File list folder

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const fs = require("fs");
-const input = process.argv;
+const path = require('path');
+const dirPath = path.join(__dirname, "files");
 
-if (input[2] == "add") {
-  fs.writeFileSync(input[3], input[4]);
-} else if (input[2] == "remove") {
-  fs.unlinkSync(input[3]);
-} else {
-  console.log("invalid output");
-}
+// for(i=0; i<5; i++){
+//     fs.writeFileSync(`${dirPath}/hello${i}.txt1`, "a simple text file")
+// }
+
+fs.readdir(dirPath, (err, files)=>{
+    files.forEach((item) =>{
+        console.log('item', item)
+        
+    })     
+})
