e77f1c9ad72002ae4f814c0ee588ccba32a549c3
Varun Sharma <varunsharma.32145@gmail.com>
2023-06-30 21:19:47 +0530

    middleware

---
+++ applicationLevelMiddleware.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+const reqFilter = (req, res, next) => {
+  if (!req.query.age) {
+    res.send("Please provide age");
+  }else if (req.query.age<18) {
+    res.send("You Cannot access this page");
+  
+  }else {
+    next();
+  }
+};
+
+app.use(reqFilter);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to home page");
+});
+app.get("/users", (req, res) => {
+  res.send("Welcome to users page");
+});
+
+app.listen(3000);
---
+++ routeLevelMiddleware.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const reqFilter = require("./middleware");
+const app = express();
+const route = express.Router();
+
+// app.use(reqFilter);
+route.use(reqFilter);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to home page");
+});
+app.get("/users", (req, res) => {
+  res.send("Welcome to users page");
+});
+route.get("/about", (req, res) => {
+  res.send("Welcome to about page");
+});
+route.get("/contact", (req, res) => {
+  res.send("Welcome to contact page");
+});
+app.use("/", route);
+app.listen(3000);
