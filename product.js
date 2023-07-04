0c7b228d914eda9c4f3c7eef35c6888df7fe7eaa
varun <varun.sharma@easygov.co.in>
2023-07-04 11:18:59 +0530

    node-mysql-start

--- product.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const productScheme = mongoose.Schema({
-    name: String,
-    price: Number,
-    brand: String,
-    category: String
-});
-
-module.exports = mongoose.model('products', productScheme);
